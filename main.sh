26c2c270ff6fbe8907f4e1c64233b9e77159e39e
Pavlos Kokozidis <paulkokos@yahoo.gr>
2019-09-24 09:23:32 +0300

    Initial commit

--- main.sh
+++ main.sh
@@ -4,3 +4,47 @@ echo "hello world"
 echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
 
+echo "The purpose of this program is to make a Graphical automation tool "
+echo "For Cpp. Feel free to contribute !!!!"
+
+# cal = calendar
+# date = date
+# who = who is logged in ant whitch terminal
+# cat
+# cd 
+# chmod
+# chown
+# curl
+# df
+# diff
+# exit 
+# shutdown
+# restart
+# find 
+# finger
+# free 
+# grep
+# groups
+# gzip
+# head
+# history
+# kill
+# less
+# ls
+# man 
+# mkdir
+# tree 
+# mv 
+# passwd
+# ping
+# ps 
+# pwd
+# ssh
+# sudo
+# tail
+# tar 
+# top
+# uname
+# w (yea this is a command !!! )
+# whoami
+# szSZX
